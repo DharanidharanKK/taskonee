ae8c45087d0b5a58c9c75f9413373ae3e0952bcf
Dharanidharan KK <138892201+DharanidharanKK@users.noreply.github.com>
2026-01-13 11:16:39 +0530

    Update config.py

--- Migration_task/config.py
+++ Migration_task/config.py
@@ -1,7 +1,8 @@
 BASE_URL = "https://apihub.document360.io/v2/Drive/Folders"
 
 HEADERS = {
-    "api_token": "1v9fUFV3l+4XZ+laMwCP8ha0HCRawfN6qQWGhVZ+rKP9ohdUBgez1Y/2cOI2Kq88P4g/eOxl1oGcZ/NG4jpERh8GGE7lHJiR1Olm0npUg0guxfPBpffculxO0yUrgdy85EX/ugGDXYEuo+voLyoraQ==",
+    "api_token": "YOUR API",
     "Content-Type": "application/json",
     "user_id": "9bc90a13-4c7a-45e6-b51d-839bc3a5a209"
 }
+
